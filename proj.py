ed88b4302df5bdaae3ef8b8f537c65102d1328fd
Lasith Eshan <168047758+eshulasi333@users.noreply.github.com>
2025-07-12 06:19:36 +0530

    MAX IN GRID

--- proj.py
+++ proj.py
@@ -1,24 +1,14 @@
-first=list(map(int,input().split()))
-z=[]
-for t in range(first[0]):
-    d=list(map(int,input().split()))
-    z.append(d)
-second=list(map(int,input().split()))
-f=[]
-for p in range(second[0]):
-    d=list(map(int,input().split()))
-    f.append(d)
-if len(z[0])!=len(f):
-    print("matrices can't multipy")
-    exit()
-b=[]
-for x in range(len(z)):
-    p=[]
-    for y in range(len(f[0])):
-        c=0
-        for a in range(len(z[0])):
-            c+=z[x][a]*f[a][y]
-        p.append(c)
-    b.append(p)
-for j in b:
-    print(j)
+size=list(map(int,input("enter row and coloumn ").split()))
+row=size[0]
+coloumn=size[1]
+mine=0
+print("enter numbers ")
+for w in range(row):
+    ty=list(map(int,input().split()))
+    if len(ty)!=coloumn:
+        print("invalid grid")
+    er=max(ty)
+    if mine<er:
+        mine=er
+print(mine)
+    
